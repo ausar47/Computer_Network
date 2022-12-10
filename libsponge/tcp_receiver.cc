db3e858c744729664d367e55b968047ac3405ffc
ausar47 <hjjausar@outlook.com>
2022-12-10 15:59:06 +0800

    lab2

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,25 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    if (!_set_syn_flag) {
+        if (!seg.header().syn)
+            return;
+        _set_syn_flag = true;
+        _isn = seg.header().seqno;
+    }
+    uint64_t absolute_ackno = _reassembler.stream_out().bytes_written() + 1;
+    uint64_t seg_absolute_seqno = unwrap(seg.header().seqno, _isn, absolute_ackno);
+    uint64_t stream_index = seg_absolute_seqno - 1 + seg.header().syn;
+    _reassembler.push_substring(seg.payload().copy(), stream_index, seg.header().fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!_set_syn_flag)
+        return nullopt;
+    uint64_t absolute_ackno = _reassembler.stream_out().bytes_written() + 1;
+    if (_reassembler.stream_out().input_ended())
+        absolute_ackno++;
+    return wrap(absolute_ackno, _isn);
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _capacity - _reassembler.stream_out().buffer_size(); }
--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32{isn + static_cast<uint32_t>(n)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +28,12 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    const constexpr uint64_t INT32_RANGE = 1ll << 32;
+    uint32_t offset = n - isn;
+    if (checkpoint > offset) {
+        uint64_t k = (checkpoint - offset + (INT32_RANGE >> 1)) / INT32_RANGE;
+        return k * INT32_RANGE + offset;
+    }
+    else
+        return offset;
 }
